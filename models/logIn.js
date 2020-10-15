a56ce5b31e1fb51f26e46fb6af04025e657ce899
Cory Manthou <63558455+cmanthou@users.noreply.github.com>
2020-10-15 18:38:59 -0400

    database code (#6)
    Co-authored-by: Nicholas Dziewit <upsidedownplatypus@gmail.com>

---
+++ models/logIn.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const logInForm = new Schema(
+    {
+        email: { type: String, required: true },
+        password: { type: String, required: true }
+    },
+);
+
+const Userlogin = mongoose.model("Userlogin", logInForm);
+
+module.exports = Userlogin;
--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const routes = require("./routes/plants");
+const userRoutes = require("./routes/logIn");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+
+require("dotenv").config();
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("client/build"));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
