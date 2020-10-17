c324067766e02791fd09f10dc3a038e744bf2adb
cpark <63162177+Conchettapark@users.noreply.github.com>
2020-10-17 19:26:39 -0400

    API to render on page (#14)
    Co-authored-by: Conchetta Park <conchettapark@MacBook-Pro.local>

--- client/src/components/layout/PlantSearch.js
+++ client/src/components/layout/PlantSearch.js
@@ -1,75 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { DebounceInput } from 'react-debounce-input';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import TREFLE_TOKEN from "./.env";
+import React, { useState, useEffect } from "react";
+import { DebounceInput } from "react-debounce-input";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
 export const PlantItem = ({
   image_url,
   scientific_name,
   common_name,
   synonyms,
-  id
+  id,
 }) => {
-
-  return (<div className="PlantItem">
-    <div id="plant-image">
-      <img src={ image_url } alt="Plant Image">
-      </img>
+  return (
+    <div className="PlantItem">
+      <div id="plant-image">
+        <img src={image_url} alt="Plant Image"></img>
+      </div>
+      <div id="plant-info">
+        <h2>{common_name}</h2>
+        <p>{scientific_name}</p>
+        {synonyms.length > 0 && <p>AKA: {synonyms.slice(0, 2).join(" or ")}</p>}
+        <p>{id}</p>
+      </div>
     </div>
-    <div id="plant-info">
-      <h2>
-        { common_name }
-        </h2>
-      <p>{ scientific_name }</p>
-        { synonyms.length > 0 && <p>AKA: { synonyms.slice(0, 2).join(' or ') }</p>}
-      <p>{ id }</p>
-      
-    </div>
-  </div>)
-}
+  );
+};
 
 export const PlantSearch = (props) => {
-
-  const [query, setQuery] = useState('Plant Name')
-  const [response, setResponse] = useState({})
+  const [query, setQuery] = useState("Plant Name");
+  const [response, setResponse] = useState({});
 
   const onQueryChange = (query) => {
-    setQuery(query)
-  }
+    setQuery(query);
+  };
 
   useEffect(() => {
     async function fetchData() {
       if (query && query.length > 0) {
-        const res = await axios.get(`/api/trefle/species/${query}`)
-        setResponse(res.data)
+        const res = await axios.get(
+          `http://localhost:5000/api/trefle/species/${query}`
+        );
+        console.log(res);
+        setResponse(res);
       }
     }
-    fetchData()
-  }, [query])
+    fetchData();
+  }, [query]);
 
-  return (<>
-          <span>
-            Plant:
-            <DebounceInput
-              minLength={3}
-              type="text"
-              onChange={e => onQueryChange(e.target.value)}
-              value={ query }
-              debounceTimeout={300}
-            />
-          </span>
+  return (
+    <>
+      <span>
+        Plant:
+        <DebounceInput
+          minLength={3}
+          type="text"
+          onChange={(e) => onQueryChange(e.target.value)}
+          value={query}
+          debounceTimeout={300}
+        />
+      </span>
       <div className="column">
-        {response.data && response.data.map(e => <PlantItem {...e} key={e.id} />)}
+        {response.data &&
+          response.data.map((e) => <PlantItem {...e} key={e.id} />)}
       </div>
-    {/* </div> */}
+    </>
+  );
+};
 
-  </>)
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   if (document.querySelectorAll("#plant-search").length > 0) {
-    const domContainer = document.querySelector('#plant-search');
+    const domContainer = document.querySelector("#plant-search");
     ReactDOM.render(React.createElement(PlantSearch), domContainer);
   }
-})
+});
--- routes/api/trefle.js
+++ routes/api/trefle.js
@@ -1,14 +1,20 @@
 const { error } = require("console");
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 require("dotenv").config();
 
-router.get("/trefle/species/:query",(req,res) => {
-    axios.get(`https://trefle.io/api/v1/species/search?token=${process.env.TREFLE_TOKEN}&q=${req.params.query}&limit=5`)
-        .then(data => {
-            console.log(data);
-            return res.json(data)
-        }).catch(error => {
-            return res.send(error)
-        }) 
-})
+router.get("/species/:query", (req, res) => {
+  axios
+    .get(
+      `https://trefle.io/api/v1/species/search?token=${process.env.TREFLE_TOKEN}&q=${req.params.query}&limit=5`
+    )
+    .then((data) => {
+      return res.json(data.data.data);
+    })
+    .catch((error) => {
+      return res.send(error);
+    });
+});
+
+module.exports = router;
