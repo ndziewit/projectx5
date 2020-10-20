2e79d1f48d047dec5380767a60b312fc51ddcc8c
Cory Manthou <63558455+cmanthou@users.noreply.github.com>
2020-10-20 18:01:10 -0400

    added garden files (#17)

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";class Landing extends Component {
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
   render() {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
