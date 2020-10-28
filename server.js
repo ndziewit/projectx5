146eaa49dae7f0ac1cf319c6596c091a236417a0
Cory Manthou <63558455+cmanthou@users.noreply.github.com>
2020-10-27 21:08:01 -0400

    added email function (#30)

--- server.js
+++ server.js
@@ -12,10 +12,23 @@ const { env } = require("process");
 
 const app = express();
 
+let nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+  host: "smtp.mail.yahoo.com", 
+    port: 465,
+    secure: true,
+  auth: {
+    user: "WooderApp@yahoo.com",
+    pass: "rqlkbgmdysxgzvnh"
+  }
+});
+
+const { compareSync } = require("bcryptjs");
+
 //Bodyparser
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: true
   })
 );
 app.use(bodyParser.json());
@@ -38,10 +51,44 @@ app.use(passport.initialize());
 //Config Passport
 require("./config/passport")(passport);
 
+// verifying the connection configuration
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages!");
+  }
+})
+  
 //Routes
 app.use("/api/trefle", trefleApi);
 app.use("/api/users", users);
 app.use("/api/garden", garden);
+
+app.post('/api/email', (req, res, next) => {
+  res.json("status")
+  var content = req.body.plant
+
+  var mail = {
+    from: "WooderApp@yahoo.com", 
+    to: "cory.manthou@gmail.com", 
+    subject: "It's Time to Water a Plant!",
+    // message: "Time to Water!",
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port} !`));
